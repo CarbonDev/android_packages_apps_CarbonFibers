LineageOS/android_packages_apps_Settings

Settings: Forward port Power menu options
Change-Id: I7dbe78e14647a79966758b75f42eb05f5429d56a

src/com/android/settings/cyanogenmod/PowerMenu.java
@@ -1,0 +1,112 @@
+/*
+ * Copyright (C) 2012 CyanogenMod
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanogenmod;
+
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class PowerMenu extends SettingsPreferenceFragment {
+    private static final String TAG = "PowerMenu";
+
+    private static final String KEY_REBOOT = "power_menu_reboot";
+    private static final String KEY_SCREENSHOT = "power_menu_screenshot";
+    private static final String KEY_PROFILES = "power_menu_profiles";
+    private static final String KEY_AIRPLANE = "power_menu_airplane";
+    private static final String KEY_SILENT = "power_menu_silent";
+
+    private CheckBoxPreference mRebootPref;
+    private CheckBoxPreference mScreenshotPref;
+    private CheckBoxPreference mProfilesPref;
+    private CheckBoxPreference mAirplanePref;
+    private CheckBoxPreference mSilentPref;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.power_menu_settings);
+
+        mRebootPref = (CheckBoxPreference) findPreference(KEY_REBOOT);
+        mRebootPref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWER_MENU_REBOOT_ENABLED, 1) == 1));
+
+        mScreenshotPref = (CheckBoxPreference) findPreference(KEY_SCREENSHOT);
+        mScreenshotPref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWER_MENU_SCREENSHOT_ENABLED, 0) == 1));
+
+        mProfilesPref = (CheckBoxPreference) findPreference(KEY_PROFILES);
+        mProfilesPref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWER_MENU_PROFILES_ENABLED, 1) == 1));
+
+        // Only enable if System Profiles are also enabled
+        boolean enabled = Settings.System.getInt(getContentResolver(),
+                Settings.System.SYSTEM_PROFILES_ENABLED, 1) == 1;
+        mProfilesPref.setEnabled(enabled);
+
+        mAirplanePref = (CheckBoxPreference) findPreference(KEY_AIRPLANE);
+        mAirplanePref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWER_MENU_AIRPLANE_ENABLED, 1) == 1));
+
+        mSilentPref = (CheckBoxPreference) findPreference(KEY_SILENT);
+        mSilentPref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWER_MENU_SILENT_ENABLED, 1) == 1));
+
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        boolean value;
+
+        if (preference == mScreenshotPref) {
+            value = mScreenshotPref.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.POWER_MENU_SCREENSHOT_ENABLED,
+                    value ? 1 : 0);
+        } else if (preference == mRebootPref) {
+            value = mRebootPref.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.POWER_MENU_REBOOT_ENABLED,
+                    value ? 1 : 0);
+        } else if (preference == mProfilesPref) {
+            value = mProfilesPref.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.POWER_MENU_PROFILES_ENABLED,
+                    value ? 1 : 0);
+       } else if (preference == mAirplanePref) {
+            value = mAirplanePref.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.POWER_MENU_AIRPLANE_ENABLED,
+                    value ? 1 : 0);
+       } else if (preference == mSilentPref) {
+            value = mSilentPref.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.POWER_MENU_SILENT_ENABLED,
+                    value ? 1 : 0);
+        } else {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+
+        return true;
+    }
+
+}

CarbonDev/android_packages_apps_CarbonFibers

Conflicts:
	AndroidManifest.xml
	res/values/strings.xml
	res/xml/system_settings.xml

src/com/carbon/settings/fragments/PowerMenu.java
@@ -1,0 +1,112 @@
+/*
+ * Copyright (C) 2012 CyanogenMod
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.carbon.settings.fragments;
+
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.carbon.settings.R;
+import com.carbon.settings.SettingsPreferenceFragment;
+
+public class PowerMenu extends SettingsPreferenceFragment {
+    private static final String TAG = "PowerMenu";
+
+    private static final String KEY_REBOOT = "power_menu_reboot";
+    private static final String KEY_SCREENSHOT = "power_menu_screenshot";
+    private static final String KEY_PROFILES = "power_menu_profiles";
+    private static final String KEY_AIRPLANE = "power_menu_airplane";
+    private static final String KEY_SILENT = "power_menu_silent";
+
+    private CheckBoxPreference mRebootPref;
+    private CheckBoxPreference mScreenshotPref;
+    private CheckBoxPreference mProfilesPref;
+    private CheckBoxPreference mAirplanePref;
+    private CheckBoxPreference mSilentPref;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.power_menu_settings);
+
+        mRebootPref = (CheckBoxPreference) findPreference(KEY_REBOOT);
+        mRebootPref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWER_MENU_REBOOT_ENABLED, 1) == 1));
+
+        mScreenshotPref = (CheckBoxPreference) findPreference(KEY_SCREENSHOT);
+        mScreenshotPref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWER_MENU_SCREENSHOT_ENABLED, 0) == 1));
+
+        mProfilesPref = (CheckBoxPreference) findPreference(KEY_PROFILES);
+        mProfilesPref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWER_MENU_PROFILES_ENABLED, 1) == 1));
+
+        // Only enable if System Profiles are also enabled
+        boolean enabled = Settings.System.getInt(getContentResolver(),
+                Settings.System.SYSTEM_PROFILES_ENABLED, 1) == 1;
+        mProfilesPref.setEnabled(enabled);
+
+        mAirplanePref = (CheckBoxPreference) findPreference(KEY_AIRPLANE);
+        mAirplanePref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWER_MENU_AIRPLANE_ENABLED, 1) == 1));
+
+        mSilentPref = (CheckBoxPreference) findPreference(KEY_SILENT);
+        mSilentPref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWER_MENU_SILENT_ENABLED, 1) == 1));
+
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        boolean value;
+
+        if (preference == mScreenshotPref) {
+            value = mScreenshotPref.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.POWER_MENU_SCREENSHOT_ENABLED,
+                    value ? 1 : 0);
+        } else if (preference == mRebootPref) {
+            value = mRebootPref.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.POWER_MENU_REBOOT_ENABLED,
+                    value ? 1 : 0);
+        } else if (preference == mProfilesPref) {
+            value = mProfilesPref.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.POWER_MENU_PROFILES_ENABLED,
+                    value ? 1 : 0);
+       } else if (preference == mAirplanePref) {
+            value = mAirplanePref.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.POWER_MENU_AIRPLANE_ENABLED,
+                    value ? 1 : 0);
+       } else if (preference == mSilentPref) {
+            value = mSilentPref.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.POWER_MENU_SILENT_ENABLED,
+                    value ? 1 : 0);
+        } else {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+
+        return true;
+    }
+
+}