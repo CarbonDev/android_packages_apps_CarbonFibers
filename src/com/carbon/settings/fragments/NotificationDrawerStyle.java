fd33e3e1454a5b0b9af6c42e9c839f41b6590e83
Daniel MM <daxx4182@gmail.com>
2013-08-15 13:28:44 +0800

    Port forward: Custom Carrier Label

--- src/com/carbon/settings/fragments/NotificationDrawerStyle.java
+++ src/com/carbon/settings/fragments/NotificationDrawerStyle.java
@@ -64,6 +64,7 @@ public class NotificationDrawerStyle extends SettingsPreferenceFragment implemen
 
     private static final String TAG = "NotificationDrawerStyle";
 
+    private static final String PREF_CUSTOM_CARRIER_LABEL = "custom_carrier_label";
     private static final String PREF_NOTIFICATION_WALLPAPER = "notification_wallpaper";
     private static final String PREF_NOTIFICATION_WALLPAPER_LANDSCAPE = "notification_wallpaper_landscape";
     private static final String PREF_NOTIFICATION_WALLPAPER_ALPHA = "notification_wallpaper_alpha";
@@ -71,6 +72,7 @@ public class NotificationDrawerStyle extends SettingsPreferenceFragment implemen
 
     private ListPreference mNotificationWallpaper;
     private ListPreference mNotificationWallpaperLandscape;
+    Preference mCustomLabel;
     SeekBarPreference mWallpaperAlpha;
     SeekBarPreference mNotifAlpha;
 
@@ -92,12 +94,15 @@ public class NotificationDrawerStyle extends SettingsPreferenceFragment implemen
         mResolver = getContentResolver();
         mActivity = getActivity();
 
-        addPreferencesFromResource(R.xml.notification_bg_pref);
+        addPreferencesFromResource(R.xml.prefs_notification);
 
         PreferenceScreen prefSet = getPreferenceScreen();
 
         customnavTemp = new File(getActivity().getFilesDir()+"/notification_wallpaper_temp.jpg");
         customnavTempLandscape = new File(getActivity().getFilesDir()+"/notification_wallpaper_temp_landscape.jpg");
+
+        mCustomLabel = prefSet.findPreference(PREF_CUSTOM_CARRIER_LABEL);
+        updateCustomLabelTextSummary();
 
         mNotificationWallpaper = (ListPreference) findPreference(PREF_NOTIFICATION_WALLPAPER);
         mNotificationWallpaper.setOnPreferenceChangeListener(this);
@@ -139,6 +144,15 @@ public class NotificationDrawerStyle extends SettingsPreferenceFragment implemen
         updateCustomBackgroundSummary();
     }
 
+    private void updateCustomLabelTextSummary() {
+        mCustomLabelText = Settings.System.getString(getActivity().getContentResolver(),
+                Settings.System.CUSTOM_CARRIER_LABEL);
+        if (mCustomLabelText == null || mCustomLabelText.length() == 0) {
+            mCustomLabel.setSummary(R.string.custom_carrier_label_notset);
+        } else {
+            mCustomLabel.setSummary(mCustomLabelText);
+        }
+    }
 
     private void updateCustomBackgroundSummary() {
         int resId;
@@ -248,6 +262,37 @@ public class NotificationDrawerStyle extends SettingsPreferenceFragment implemen
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mCustomLabel) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+
+            alert.setTitle(R.string.custom_carrier_label_title);
+            alert.setMessage(R.string.custom_carrier_label_explain);
+
+            // Set an EditText view to get user input
+            final EditText input = new EditText(getActivity());
+            input.setText(mCustomLabelText != null ? mCustomLabelText : "");
+            alert.setView(input);
+
+            alert.setPositiveButton(getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    String value = ((Spannable) input.getText()).toString();
+                    Settings.System.putString(getActivity().getContentResolver(),
+                            Settings.System.CUSTOM_CARRIER_LABEL, value);
+                    updateCustomLabelTextSummary();
+                    Intent i = new Intent();
+                    i.setAction("com.carbon.settings.LABEL_CHANGED");
+                    mContext.sendBroadcast(i);
+                }
+            });
+            alert.setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    // Canceled.
+                }
+            });
+
+            alert.show();
+        }
+
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
