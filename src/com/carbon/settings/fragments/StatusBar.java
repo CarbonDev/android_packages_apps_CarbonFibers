029e047eb21d56d1985a883fad025113ca1a7d11
Daniel MM <daxx4182@gmail.com>
2013-08-24 16:28:57 +0800

    Add status icon color
    cleanup statusbar java using our preferencefragment too

--- src/com/carbon/settings/fragments/StatusBar.java
+++ src/com/carbon/settings/fragments/StatusBar.java
@@ -32,6 +32,7 @@ import com.carbon.settings.R;
 import com.carbon.settings.SettingsPreferenceFragment;
 import com.carbon.settings.Utils;
 import com.carbon.settings.util.Helpers;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 public class StatusBar extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
@@ -44,10 +45,14 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
     private static final String UI_COLLAPSE_BEHAVIOUR = "notification_drawer_collapse_on_dismiss";
     private static final CharSequence STATUS_BAR_BEHAVIOR = "status_bar_behavior";
     private static final String STATUS_BAR_QUICK_PEEK = "status_bar_quick_peek";
+    private static final String STATUS_ICON_COLOR_BEHAVIOR = "status_icon_color_behavior";
+    private static final String STATUS_ICON_COLOR = "status_icon_color";
 
     private ListPreference mStatusBarCmSignal;
     private CheckBoxPreference mStatusBarNotifCount;
     private CheckBoxPreference mStatusbarSliderPreference;
+    private CheckBoxPreference mStatusIconBehavior;
+    private ColorPickerPreference mIconColor;
     private PreferenceScreen mClockStyle;
     private PreferenceCategory mPrefCategoryGeneral;
     private ListPreference mCollapseOnDismiss;
@@ -56,35 +61,31 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
 
     private static int mBarBehavior;
 
-    private static ContentResolver mContentResolver;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.status_bar);
 
-        ContentResolver resolver = getActivity().getContentResolver();
         PreferenceScreen prefSet = getPreferenceScreen();
-        mContentResolver = getContentResolver();
 
         mStatusBarCmSignal = (ListPreference) prefSet.findPreference(STATUS_BAR_SIGNAL);
 
-        int signalStyle = Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+        int signalStyle = Settings.System.getInt(mContentAppRes,
                 Settings.System.STATUS_BAR_SIGNAL_TEXT, 0);
         mStatusBarCmSignal.setValue(String.valueOf(signalStyle));
         mStatusBarCmSignal.setSummary(mStatusBarCmSignal.getEntry());
         mStatusBarCmSignal.setOnPreferenceChangeListener(this);
 
         mStatusBarNotifCount = (CheckBoxPreference) prefSet.findPreference(STATUS_BAR_NOTIF_COUNT);
-        mStatusBarNotifCount.setChecked((Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+        mStatusBarNotifCount.setChecked((Settings.System.getInt(mContentAppRes,
                 Settings.System.STATUS_BAR_NOTIF_COUNT, 0) == 1));
 
         mStatusbarSliderPreference = (CheckBoxPreference) findPreference(STATUS_BAR_BRIGHTNESS);
-        mStatusbarSliderPreference.setChecked((Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+        mStatusbarSliderPreference.setChecked((Settings.System.getInt(mContentAppRes,
                 Settings.System.STATUSBAR_BRIGHTNESS_SLIDER, 0) == 1));
 
-        int collapseBehaviour = Settings.System.getInt(resolver,
+        int collapseBehaviour = Settings.System.getInt(mContentRes,
                 Settings.System.STATUS_BAR_COLLAPSE_ON_DISMISS,
                 Settings.System.STATUS_BAR_COLLAPSE_IF_NO_CLEARABLE);
         mCollapseOnDismiss = (ListPreference) prefSet.findPreference(UI_COLLAPSE_BEHAVIOUR);
@@ -93,16 +94,23 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
         updateCollapseBehaviourSummary(collapseBehaviour);
 
         mStatusBarBeh = (ListPreference) findPreference(STATUS_BAR_BEHAVIOR);
-        int mBarBehavior = Settings.System.getInt(mContentResolver,
+        int mBarBehavior = Settings.System.getInt(mContentRes,
                 Settings.System.HIDE_STATUSBAR, 0);
-        mStatusBarBeh.setValue(Integer.toString(Settings.System.getInt(mContentResolver,
+        mStatusBarBeh.setValue(Integer.toString(Settings.System.getInt(mContentRes,
                 Settings.System.HIDE_STATUSBAR, mBarBehavior)));
         updateStatusBarBehaviorSummary(mBarBehavior);
         mStatusBarBeh.setOnPreferenceChangeListener(this);
 
         mStatusBarQuickPeek = (CheckBoxPreference) prefSet.findPreference(STATUS_BAR_QUICK_PEEK);
-        mStatusBarQuickPeek.setChecked((Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+        mStatusBarQuickPeek.setChecked((Settings.System.getInt(mContentAppRes,
                 Settings.System.STATUSBAR_PEEK, 0) == 1));
+
+        mStatusIconBehavior = (CheckBoxPreference) prefSet.findPreference(STATUS_ICON_COLOR_BEHAVIOR);
+        mStatusIconBehavior.setChecked(Settings.System.getInt(mContentAppRes,
+                Settings.System.ICON_COLOR_BEHAVIOR, 0) == 1);
+
+        mIconColor = (ColorPickerPreference) findPreference(STATUS_ICON_COLOR);
+        mIconColor.setOnPreferenceChangeListener(this);
 
         mPrefCategoryGeneral = (PreferenceCategory) findPreference(STATUS_BAR_CATEGORY_GENERAL);
 
@@ -124,29 +132,37 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getActivity().getContentResolver();
         boolean result = false;
 
         if (preference == mStatusBarCmSignal) {
             int signalStyle = Integer.valueOf((String) newValue);
             int index = mStatusBarCmSignal.findIndexOfValue((String) newValue);
-            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+            Settings.System.putInt(mContentAppRes,
                     Settings.System.STATUS_BAR_SIGNAL_TEXT, signalStyle);
             mStatusBarCmSignal.setSummary(mStatusBarCmSignal.getEntries()[index]);
             return true;
         } else if (preference == mCollapseOnDismiss) {
             int value = Integer.valueOf((String) newValue);
-            Settings.System.putInt(resolver,
+            Settings.System.putInt(mContentRes,
                     Settings.System.STATUS_BAR_COLLAPSE_ON_DISMISS, value);
             updateCollapseBehaviourSummary(value);
             return true;
         } else if (preference == mStatusBarBeh) {
             int mBarBehavior = Integer.valueOf((String) newValue);
             int index = mStatusBarBeh.findIndexOfValue((String) newValue);
-            Settings.System.putInt(getActivity().getContentResolver(),
+            Settings.System.putInt(mContentRes,
                     Settings.System.HIDE_STATUSBAR, mBarBehavior);
             mStatusBarBeh.setSummary(mStatusBarBeh.getEntries()[index]);
             updateStatusBarBehaviorSummary(mBarBehavior);
+            Helpers.restartSystemUI();
+            return true;
+        } else if (preference == mIconColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mContentAppRes,
+                    Settings.System.STATUS_ICON_COLOR, intHex);
             Helpers.restartSystemUI();
             return true;
         }
@@ -159,17 +175,22 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
 
         if (preference == mStatusBarNotifCount) {
             value = mStatusBarNotifCount.isChecked();
-            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+            Settings.System.putInt(mContentAppRes,
                     Settings.System.STATUS_BAR_NOTIF_COUNT, value ? 1 : 0);
             return true;
+        } else if (preference == mStatusIconBehavior) {
+            Settings.System.putInt(mContentAppRes,
+                    Settings.System.ICON_COLOR_BEHAVIOR,
+            mStatusIconBehavior.isChecked() ? 1 : 0);
+            Helpers.restartSystemUI();
         } else if (preference == mStatusbarSliderPreference) {
             value = mStatusbarSliderPreference.isChecked();
-            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+            Settings.System.putInt(mContentAppRes,
                     Settings.System.STATUSBAR_BRIGHTNESS_SLIDER, value ? 1 : 0);
             return true;
         } else if (preference == mStatusBarQuickPeek) {
             value = mStatusBarQuickPeek.isChecked();
-            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+            Settings.System.putInt(mContentAppRes,
                     Settings.System.STATUSBAR_PEEK, value ? 1 : 0);
             return true;
         }
@@ -177,7 +198,7 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
     }
 
     private void updateClockStyleDescription() {
-        if (Settings.System.getInt(getActivity().getContentResolver(),
+        if (Settings.System.getInt(mContentRes,
                Settings.System.STATUS_BAR_CLOCK, 1) == 1) {
             mClockStyle.setSummary(getString(R.string.clock_enabled));
         } else {
