0ebeeacbf3b7c4ee3003bd4abfc57d9c45b80441
Daniel MM <daxx4182@gmail.com>
2013-09-06 11:20:17 +0800

    Device Tools: Improvements
    - Make it more similar like CM's Apollo
    
    TODO:
    - Add Samsung's msm8960 support

--- src/com/carbon/settings/device/DeviceTools.java
+++ src/com/carbon/settings/device/DeviceTools.java
@@ -17,14 +17,16 @@
 package com.carbon.settings.device;
 
 import android.app.ActionBar;
-import android.app.ActionBar.Tab;
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.res.Resources;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.view.MenuItem;
 import android.util.Log;
@@ -44,116 +46,70 @@ public class DeviceTools extends FragmentActivity {
     public static final String SHARED_PREFERENCES_BASENAME = "com.carbon.settings.device";
     public static final String ACTION_UPDATE_PREFERENCES = "com.carbon.settings.device.UPDATE";
 
+    PagerTabStrip mPagerTabStrip;
     ViewPager mViewPager;
-    TabsAdapter mTabsAdapter;
+
+    boolean isHtcOne;
+    String titleString[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.pager_tab);
 
-        mViewPager = new ViewPager(this);
-        mViewPager.setId(R.id.viewPager);
-        setContentView(mViewPager);
+        isHtcOne = getResources().getBoolean(R.bool.is_htc_one);
+
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        mPagerTabStrip = (PagerTabStrip) findViewById(R.id.pagerTabStrip);
+        TitleAdapter titleAdapter = new TitleAdapter(getFragmentManager());
+        mViewPager.setAdapter(titleAdapter);
+        mViewPager.setCurrentItem(0);
+        mPagerTabStrip.setDrawFullUnderline(true);
+        mPagerTabStrip.setTabIndicatorColor(getResources().getColor(android.R.color.holo_blue_light));
 
         final ActionBar bar = getActionBar();
-        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        bar.setDisplayOptions(ActionBar.DISPLAY_SHOW_TITLE, ActionBar.DISPLAY_SHOW_TITLE);
-        bar.setTitle(R.string.device_tools_title);
         bar.setDisplayHomeAsUpEnabled(true);
 
-        final boolean isHtcOne = getResources().getBoolean(R.bool.is_htc_one);
-
-        mTabsAdapter = new TabsAdapter(this, mViewPager);
-        if (isHtcOne) {
-            mTabsAdapter.addTab(bar.newTab().setText(R.string.category_buttonlight_title),
-                        ButtonLightFragmentActivity.class, null);
-            mTabsAdapter.addTab(bar.newTab().setText(R.string.category_sensors_title),
-                        SensorsFragmentActivity.class, null);
-            mTabsAdapter.addTab(bar.newTab().setText(R.string.category_touchscreen_title),
-                        TouchscreenFragmentActivity.class, null);
-        }
-
-        if (savedInstanceState != null) {
-            bar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));
-        }
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt("tab", getActionBar().getSelectedNavigationIndex());
-    }
+    class TitleAdapter extends FragmentPagerAdapter {
+        String titles[] = getTitles();
+        private Fragment frags[] = new Fragment[titles.length];
 
-    public static class TabsAdapter extends FragmentPagerAdapter
-            implements ActionBar.TabListener, ViewPager.OnPageChangeListener {
-        private final Context mContext;
-        private final ActionBar mActionBar;
-        private final ViewPager mViewPager;
-        private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
-
-        static final class TabInfo {
-            private final Class<?> clss;
-            private final Bundle args;
-
-            TabInfo(Class<?> _class, Bundle _args) {
-                clss = _class;
-                args = _args;
+        public TitleAdapter(FragmentManager fm) {
+            super(fm);
+            // Display for certain devices only
+            if (isHtcOne) {
+                frags[0] = new ButtonLightFragmentActivity();
+                frags[1] = new SensorsFragmentActivity();
+                frags[2] = new TouchscreenFragmentActivity();
             }
         }
 
-        public TabsAdapter(Activity activity, ViewPager pager) {
-            super(activity.getFragmentManager());
-            mContext = activity;
-            mActionBar = activity.getActionBar();
-            mViewPager = pager;
-            mViewPager.setAdapter(this);
-            mViewPager.setOnPageChangeListener(this);
-        }
-
-        public void addTab(ActionBar.Tab tab, Class<?> clss, Bundle args) {
-            TabInfo info = new TabInfo(clss, args);
-            tab.setTag(info);
-            tab.setTabListener(this);
-            mTabs.add(info);
-            mActionBar.addTab(tab);
-            notifyDataSetChanged();
-        }
-
         @Override
-        public int getCount() {
-            return mTabs.size();
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
         }
 
         @Override
         public Fragment getItem(int position) {
-            TabInfo info = mTabs.get(position);
-            return Fragment.instantiate(mContext, info.clss.getName(), info.args);
+            return frags[position];
         }
 
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        @Override
+        public int getCount() {
+            return frags.length;
         }
+    }
 
-        public void onPageSelected(int position) {
-            mActionBar.setSelectedNavigationItem(position);
+    private String[] getTitles() {
+        if (isHtcOne) {
+            titleString = new String[]{
+                    getResources().getString(R.string.category_buttonlight_title),
+                    getResources().getString(R.string.category_sensors_title),
+                    getResources().getString(R.string.category_touchscreen_title)};
         }
-
-        public void onPageScrollStateChanged(int state) {
-        }
-
-        public void onTabSelected(Tab tab, FragmentTransaction ft) {
-            Object tag = tab.getTag();
-            for (int i=0; i<mTabs.size(); i++) {
-                if (mTabs.get(i) == tag) {
-                    mViewPager.setCurrentItem(i);
-                }
-            }
-        }
-
-        public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-        }
-
-        public void onTabReselected(Tab tab, FragmentTransaction ft) {
-        }
+        return titleString;
     }
 
     @Override
