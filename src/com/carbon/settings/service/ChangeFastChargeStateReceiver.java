9d7388b67fb445db51b6b6897f321c7506b3ca3f
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-09-13 09:19:15 +0200

    Show a Toast message if fastcharge is not supported
    Change-Id: I6b13c57de117b0284949c74d0899020739a04d69
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>
    
    Conflicts:
    	res/values/strings.xml
    	src/com/carbon/settings/service/ChangeFastChargeStateReceiver.java

--- src/com/carbon/settings/service/ChangeFastChargeStateReceiver.java
+++ src/com/carbon/settings/service/ChangeFastChargeStateReceiver.java
@@ -4,8 +4,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.File;
+
+import com.carbon.settings.R;
 
 public class ChangeFastChargeStateReceiver extends BroadcastReceiver {
 
@@ -15,6 +18,9 @@ public class ChangeFastChargeStateReceiver extends BroadcastReceiver {
                 .getString(com.android.internal.R.string.config_fastChargePath);
         if (fCHargePath == null || fCHargePath.isEmpty() || !new File(fCHargePath).exists()) {
             Log.e("CR_Settings", "Attempted to change fast charge state but it's not enabled?");
+            Toast.makeText(context, context.getString(R.string.fast_charge_not_supported),
+                    Toast.LENGTH_SHORT).show();
+            Log.e("CarbonFibers", "Attempted to change fast charge state but it's not enabled?");
             return;
         }
         final String value = intent.getBooleanExtra("newState", false) ? "1" : "0";
