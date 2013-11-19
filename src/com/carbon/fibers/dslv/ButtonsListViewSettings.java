494b8d4ecd20f1a4aa919d443ea4d3d8ae5a3611
Lars Greiss <kufikugel@googlemail.com>
2013-11-19 08:16:08 +0100

    settings: Slims new Power Menu (2/2)
    gmillz already prepared it and I finished it now. Idea was to be more
    flexible on the power menu but at the same time not complicated.
    So we adapted Power Menu to our dslv system which allows to sort
    drag swipe etc the entries.
    
    So user can now have the power menu how they like it.
    As well custom apps or activities can be added
    
    So currently:
    airplanemode
    custom apps
    screenshot
    reboot
    sound
    power off
    
    are available as options to add or remove or customize with our
    easy dslv settings
    
    As well we are only using our central classes for it....means adding
    whatever shortcut is mostly only a 10 line change in future
    
    Change-Id: I8a3f99bdd0ffeab84b8b4067562d81f09c862273

--- src/com/carbon/fibers/dslv/ButtonsListViewSettings.java
+++ src/com/carbon/fibers/dslv/ButtonsListViewSettings.java
@@ -59,6 +59,7 @@ import com.android.internal.util.slim.ButtonsHelper;
 import com.android.internal.util.slim.ImageHelper;
 import com.android.internal.util.slim.DeviceUtils;
 import com.android.internal.util.slim.DeviceUtils.FilteredDeviceFeaturesArray;
+import com.android.internal.util.slim.PolicyHelper;
 
 import com.carbon.fibers.preference.SettingsPreferenceFragment;
 import com.carbon.fibers.R;
@@ -89,7 +90,7 @@ public class ButtonsListViewSettings extends ListFragment implements
     private static final int PIE_SECOND            = 2;
     private static final int NAV_RING              = 3;
     private static final int LOCKSCREEN_SHORTCUT   = 4;
-
+    private static final int POWER_MENU_SHORTCUT   = 5;
 
     private static final int DEFAULT_MAX_BUTTON_NUMBER = 5;
 
@@ -482,6 +483,9 @@ public class ButtonsListViewSettings extends ListFragment implements
             case PIE_SECOND:
                 return ButtonsHelper.getPieSecondLayerConfigWithDescription(
                     mActivity, mActionValuesKey, mActionEntriesKey);
+            case POWER_MENU_SHORTCUT:
+                return PolicyHelper.getPowerMenuConfigWithDescription(
+                    mActivity, mActionValuesKey, mActionEntriesKey);
             case LOCKSCREEN_SHORTCUT:
                 return ButtonsHelper.getLockscreenShortcutConfig(mActivity);
         }
@@ -501,6 +505,9 @@ public class ButtonsListViewSettings extends ListFragment implements
                 break;
             case PIE_SECOND:
                 ButtonsHelper.setPieSecondLayerConfig(mActivity, buttonConfigs, reset);
+                break;
+            case POWER_MENU_SHORTCUT:
+                PolicyHelper.setPowerMenuConfig(mActivity, buttonConfigs, reset);
                 break;
             case LOCKSCREEN_SHORTCUT:
                 ButtonsHelper.setLockscreenShortcutConfig(mActivity, buttonConfigs, reset);
@@ -548,10 +555,17 @@ public class ButtonsListViewSettings extends ListFragment implements
                     getResources().getString(R.string.shortcut_action_longpress)
                     + " " + getItem(position).getLongpressActionDescription());
             }
-            holder.iconView.setImageDrawable(ImageHelper.resize(
-                    mActivity, ButtonsHelper.getButtonIconImage(mActivity,
-                    getItem(position).getClickAction(),
-                    getItem(position).getIcon()), 36));
+            if (mButtonMode == POWER_MENU_SHORTCUT) {
+                holder.iconView.setImageDrawable(ImageHelper.resize(
+                        mActivity, PolicyHelper.getPowerMenuIconImage(mActivity,
+                        getItem(position).getClickAction(),
+                        getItem(position).getIcon(), false), 36));
+            } else {
+                holder.iconView.setImageDrawable(ImageHelper.resize(
+                        mActivity, ButtonsHelper.getButtonIconImage(mActivity,
+                        getItem(position).getClickAction(),
+                        getItem(position).getIcon()), 36));
+            }
 
             if (!mDisableIconPicker && holder.iconView.getDrawable() != null) {
                 holder.iconView.setOnClickListener(new OnClickListener() {
@@ -645,6 +659,9 @@ public class ButtonsListViewSettings extends ListFragment implements
                     switch (getOwner().mButtonMode) {
                         // case LOCKSCREEN_SHORTCUT:
                         case LOCKSCREEN_SHORTCUT:
+                        case POWER_MENU_SHORTCUT:
+                            buttonMode = res.getString(R.string.shortcut_action_help_shortcut);
+                            break;
                         case NAV_BAR:
                         case NAV_RING:
                         case PIE:
