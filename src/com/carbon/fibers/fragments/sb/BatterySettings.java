21e7c730886f0e3cc2dbde931f8f9246fa724d4a
Robert Rascon <rrascon22@gmail.com>
2014-03-14 13:32:42 -0500

    Revert "CFibers: Ability to hide battery icon on statusbar"
    This reverts commit 57830f7d8016dd329296293f1e7e0eccb99dd27d.
    
    Change-Id: I2c46b9f9d512fa3803c032e44d444453d3d0ee30

--- src/com/carbon/fibers/fragments/sb/BatterySettings.java
+++ src/com/carbon/fibers/fragments/sb/BatterySettings.java
@@ -275,11 +275,6 @@ public class BatterySettings extends SettingsPreferenceFragment
             mBatteryTextColor.setEnabled(true);
             mBatteryTextChargingColor.setEnabled(true);
             mCircleAnimSpeed.setEnabled(true);
-        } else if (batteryIconStat == 7) {
-            mBatteryColor.setEnabled(false);
-            mBatteryTextColor.setEnabled(false);
-            mBatteryTextChargingColor.setEnabled(false);
-            mCircleAnimSpeed.setEnabled(false);
         } else {
             mBatteryColor.setEnabled(false);
             mBatteryTextColor.setEnabled(true);
