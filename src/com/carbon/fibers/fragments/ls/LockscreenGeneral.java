ce5e71ca8649b43805b79b07f097c5d3357587f6
Andros Rosa <andros2011@gmail.com>
2014-03-17 12:57:39 -0400

    Move lock before unlock to Security settings
    Revert "CarbonFibers: Lock before unlock the new way (2/2)"
    
    This reverts commit b6f97147ee2544ea544b8b4ce52ed3e3f58375c6.
    
    Change-Id: Ide63aaff0125f2a6fbf4c34c762360b1ec96b188

--- src/com/carbon/fibers/fragments/ls/LockscreenGeneral.java
+++ src/com/carbon/fibers/fragments/ls/LockscreenGeneral.java
@@ -83,8 +83,6 @@ public class LockscreenGeneral extends SettingsPreferenceFragment implements
 
     private static final String BATTERY_AROUND_LOCKSCREEN_RING = "battery_around_lockscreen_ring";
 
-    private static final String LOCK_BEFORE_UNLOCK = "lock_before_unlock";
-
     private ListPreference mBatteryStatus;
     private CheckBoxPreference mSeeThrough;
     private SeekBarPreference mBlurRadius;
@@ -101,8 +99,6 @@ public class LockscreenGeneral extends SettingsPreferenceFragment implements
     private ContentResolver mResolver;
 
     private IKeyguardService mKeyguardService;
-
-    private CheckBoxPreference mLockBeforeUnlock;
 
     private final ServiceConnection mKeyguardConnection = new ServiceConnection() {
         @Override
@@ -182,16 +178,6 @@ public class LockscreenGeneral extends SettingsPreferenceFragment implements
         mSelectLockscreenWallpaper = findPreference(KEY_SELECT_LOCKSCREEN_WALLPAPER);
         mSelectLockscreenWallpaper.setEnabled(mLockscreenWallpaper.isChecked());
         mWallpaperTemporary = new File(getActivity().getCacheDir() + "/lockwallpaper.tmp");
-
-        // Lock before Unlock
-        mLockBeforeUnlock = (CheckBoxPreference) findPreference(LOCK_BEFORE_UNLOCK);
-        if (mLockBeforeUnlock != null) {
-            mLockBeforeUnlock.setChecked(
-                    Settings.Secure.getInt(getContentResolver(),
-                    Settings.Secure.LOCK_BEFORE_UNLOCK, 0) == 1);
-            mLockBeforeUnlock.setOnPreferenceChangeListener(this);
-        }
-
     }
 
     public void onActivityResult(int requestCode, int resultCode,
@@ -291,12 +277,6 @@ public class LockscreenGeneral extends SettingsPreferenceFragment implements
         } else if (preference == mGlowpadTorch) {
             Settings.System.putInt(getContentResolver(),
                     Settings.System.LOCKSCREEN_GLOWPAD_TORCH,
-                    (Boolean) objValue ? 1 : 0);
-            return true;
-
-        } else if (preference == mLockBeforeUnlock) {
-            Settings.Secure.putInt(getContentResolver(),
-                    Settings.Secure.LOCK_BEFORE_UNLOCK,
                     (Boolean) objValue ? 1 : 0);
             return true;
         }
