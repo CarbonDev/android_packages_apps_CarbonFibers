SlimRoms/packages_apps_Settings

QS: add theme switcher tile (TRDS) 2/2
this adds a theme switcher tile

normal click: turn on/off dark/light theme
longpress: switch between the auto modes (manual/light sensor/twilight)

Change-Id: I5f4bb146e240bf31b6486aeff85b0828ee6fbb09

src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
@@ -42,6 +42,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_SCREENTIMEOUT;
 import static com.android.internal.util.slim.QSConstants.TILE_SETTINGS;
 import static com.android.internal.util.slim.QSConstants.TILE_SLEEP;
 import static com.android.internal.util.slim.QSConstants.TILE_SYNC;
+import static com.android.internal.util.slim.QSConstants.TILE_THEME;
 import static com.android.internal.util.slim.QSConstants.TILE_TORCH;
 import static com.android.internal.util.slim.QSConstants.TILE_USBTETHER;
 import static com.android.internal.util.slim.QSConstants.TILE_USER;
@@ -151,6 +152,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_REBOOT, R.string.title_tile_reboot,
                 "com.android.systemui:drawable/ic_qs_reboot"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_THEME, R.string.title_tile_theme,
+                "com.android.systemui:drawable/ic_qs_theme_manual"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {

CarbonDev/android_packages_apps_CarbonFibers

Signed-off-by: MURA Stefano [STELIX PippoX3] <ssspinni@gmail.com>

src/com/carbon/fibers/fragments/sb/QuickSettingsUtil.java
@@ -48,6 +48,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_USER;
 import static com.android.internal.util.cm.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFI;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFIAP;
+import static com.android.internal.util.cm.QSConstants.TILE_THEME;
 import static com.android.internal.util.cm.QSConstants.TILE_QUICKRECORD;
 
 import android.content.ContentResolver;
@@ -163,6 +164,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_MUSIC, R.string.title_tile_music,
                 "com.android.systemui:drawable/ic_qs_media_pause"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_THEME, R.string.title_tile_theme,
+                "com.android.systemui:drawable/ic_qs_theme_manual"));
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_SCREENSHOT, R.string.title_tile_screenshot,
                 "com.android.systemui:drawable/ic_qs_screenshot"));